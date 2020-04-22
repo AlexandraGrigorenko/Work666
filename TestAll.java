e51076076eb104283b36b6b13767612826233547
AlexandraGrigorenko <60101421+AlexandraGrigorenko@users.noreply.github.com>
2020-04-22 14:34:29 +0300

    Add files via upload

---
+++ TestAll.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({MassTestArray1.class,
+        MassTestArray2.class,
+        TestArray.class} )
+
+public class TestAll {
+}
